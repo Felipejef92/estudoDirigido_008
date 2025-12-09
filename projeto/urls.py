a4030f8793bfdf468507c567fe3635a1d0b11aa3
Felipejef92 <felipedasilvaquintal22@gmail.com>
2025-12-09 20:48:34 -0300

    Ajustar URLs e configurações do swagger

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,23 +1,5 @@
-"""
-URL configuration for projeto project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.schemas import get_schema_view
-from django.views.generic import TemplateView
 from core.views import home
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -25,18 +7,10 @@ urlpatterns = [
     path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
-    path("openapi/", SpectacularAPIView.as_view(), name="openapi-schema"),
-    path("docs/", SpectacularSwaggerView.as_view(), name="swagger-ui"),
 
-    # Swagger
-    path("openapi/", get_schema_view(
-        title="PATRI-TECH API",
-        description="Documentação da API de inventário de bens",
-        version="1.0.0"
-    ), name="openapi-schema"),
+    # OpenAPI Schema (drf-spectacular)
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
 
-    path("docs/", TemplateView.as_view(
-        template_name="swagger.html"
-    ), name="swagger-ui"),
+    # Swagger UI (drf-spectacular)
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
-
